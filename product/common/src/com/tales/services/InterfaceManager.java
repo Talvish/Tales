a2cadb6c2e9eee9cf44710185bdd41ce16392f66
Joseph Molnar <josephmolnar@hotmail.com>
2014-02-14 23:04:22 -0800

    update the interface manager to ensure start/stop/suspend/resume are called in guaranteed order.

--- product/common/src/com/tales/services/InterfaceManager.java
+++ product/common/src/com/tales/services/InterfaceManager.java
@@ -15,14 +15,15 @@
 // ***************************************************************************
 package com.tales.services;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import com.google.common.base.Preconditions;
 import com.google.common.base.Strings;
-
 import com.tales.system.ExecutionLifecycleListener;
 import com.tales.system.ExecutionLifecycleListeners;
 import com.tales.system.ExecutionLifecycleState;
@@ -33,9 +34,13 @@ import com.tales.system.ExecutionLifecycleState;
  *
  */
 public class InterfaceManager {
-	private Map<String, Interface> interfaces 	= new HashMap<String, Interface>( 0 );
+	//       can and need to do work prior to others, think queue serivces where you want to
+	//       absorbed 
+	
+	private List<Interface> interfaceList				= new ArrayList<Interface>( 0 );
+	private Map<String, Interface> interfaceMap			= new HashMap<String, Interface>( 0 );
 	private ExecutionLifecycleState lifecycleState		= ExecutionLifecycleState.CREATED;
-	private final Object interfacesLock 		= new Object( );
+	private final Object interfacesLock 				= new Object( );
 	private final ExecutionLifecycleListeners listeners	= new ExecutionLifecycleListeners( ); 
 	
 	/**
@@ -45,7 +50,7 @@ public class InterfaceManager {
 	 */
 	public Interface getInterface( String theName ) {
 		Preconditions.checkArgument( !Strings.isNullOrEmpty( theName ), "need the name of the interface");
-		return this.interfaces.get( theName );
+		return this.interfaceMap.get( theName );
 	}
 	
 	/**
@@ -53,7 +58,7 @@ public class InterfaceManager {
 	 * @return the list of interfaces
 	 */
 	public Collection<Interface> getInterfaces( ) {
-		return this.interfaces.values();
+		return interfaceList;
 	}
 	
 	/**
@@ -73,13 +78,13 @@ public class InterfaceManager {
 	}
 	
 	/**
-	 * Starts all of the interfaces.
+	 * Starts all of the interfaces in order they were added.
 	 */
 	public void start( ) {
 		Preconditions.checkState( this.lifecycleState == ExecutionLifecycleState.CREATED, "Cannot start the interfaces when the status is '%s'.", this.lifecycleState );
 		this.lifecycleState = ExecutionLifecycleState.STARTING;
 		this.listeners.onStarting( this, this.lifecycleState );
-		for( Interface serviceInterface: interfaces.values() ) {
+		for( Interface serviceInterface: interfaceList ) {
 			serviceInterface.start();
 		}
 		this.lifecycleState = ExecutionLifecycleState.STARTED;
@@ -89,13 +94,15 @@ public class InterfaceManager {
 	}
 	
 	/**
-	 * Stops all the interfaces.
+	 * Stops all the interfaces in the opposite order from how they were added.
 	 */
 	public void stop( ) {
 		Preconditions.checkState( this.lifecycleState == ExecutionLifecycleState.STARTED || this.lifecycleState == ExecutionLifecycleState.RUNNING || this.lifecycleState == ExecutionLifecycleState.SUSPENDED, "Cannot stop the interfaces when the status is '%s'.", this.lifecycleState );
 		this.lifecycleState = ExecutionLifecycleState.STOPPING;
 		this.listeners.onStopping( this, this.lifecycleState );
-		for( Interface serviceInterface: interfaces.values() ) {
+		List<Interface> serviceInterfaces = this.interfaceList;
+		for( int index = serviceInterfaces.size() - 1; index >= 0; index -= 1 ) {
+			Interface serviceInterface = serviceInterfaces.get( index );
 			serviceInterface.stop();
 		}
 		this.lifecycleState = ExecutionLifecycleState.STOPPED;
@@ -104,12 +111,15 @@ public class InterfaceManager {
 	
 	/**
 	 * This is call to suspend all associated interfaces except the admin interface.
+	 * They are called in the opposite order they were added.
 	 */
 	public void suspend( ) {
 		Preconditions.checkState( canSuspend( ), "Cannot suspend when the status is '%s'.", this.lifecycleState );
 		this.lifecycleState = ExecutionLifecycleState.SUSPENDED;
 		this.listeners.onSuspended( this, this.lifecycleState );
-		for( Interface serviceInterface: interfaces.values() ) {
+		List<Interface> serviceInterfaces = this.interfaceList;
+		for( int index = serviceInterfaces.size() - 1; index >= 0; index -= 1 ) {
+			Interface serviceInterface = serviceInterfaces.get( index );
 			if( !serviceInterface.getName().equals( "admin" ) && serviceInterface.canSuspend( ) ) {
 				serviceInterface.suspend();
 			}
@@ -118,12 +128,13 @@ public class InterfaceManager {
 	
 	/**
 	 * This is called to resume the previously suspended interfaces.
+	 * They are called in the order they were added.
 	 */
 	public void resume( ) {
 		Preconditions.checkState( canResume( ), "Cannot resume when the status is '%s'.", this.lifecycleState );
 		this.lifecycleState = ExecutionLifecycleState.RUNNING;
 		this.listeners.onRunning( this, this.lifecycleState );
-		for( Interface serviceInterface: interfaces.values() ) {
+		for( Interface serviceInterface: interfaceList ) {
 			if( serviceInterface.canResume( ) ) {
 				serviceInterface.resume();
 			}
@@ -156,11 +167,15 @@ public class InterfaceManager {
 		Preconditions.checkState( this.lifecycleState == ExecutionLifecycleState.CREATED || this.lifecycleState == ExecutionLifecycleState.STARTING, "Cannot register an interface when the status is '%s'.", this.lifecycleState );
 		Preconditions.checkNotNull( theInterface );
 		synchronized( this.interfacesLock ) {
-			Preconditions.checkArgument( !this.interfaces.containsKey( theInterface.getName( ) ), String.format( "An interface named '%s' already exists.", theInterface.getName( ) ) );
+			Preconditions.checkArgument( !this.interfaceMap.containsKey( theInterface.getName( ) ), String.format( "An interface named '%s' already exists.", theInterface.getName( ) ) );
 
-			HashMap<String, Interface> newInterfaces = new HashMap<String,Interface>( this.interfaces );
-			newInterfaces.put( theInterface.getName(), theInterface );
-			interfaces = Collections.unmodifiableMap( newInterfaces );
+			HashMap<String, Interface> newInterfaceMap = new HashMap<>( this.interfaceMap );
+			newInterfaceMap.put( theInterface.getName(), theInterface );
+			interfaceMap = Collections.unmodifiableMap( newInterfaceMap );
+			
+			List<Interface> newInterfaceList = new ArrayList<>( this.interfaceList );
+			newInterfaceList.add( theInterface );
+			interfaceList = Collections.unmodifiableList( newInterfaceList );
 		}
 	}
 }
