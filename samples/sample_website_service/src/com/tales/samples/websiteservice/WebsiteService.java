b9b62366ac2b4fa627f0067e51b65786cb568102
Joseph Molnar <josephmolnar@hotmail.com>
2014-08-18 00:28:57 -0700

    added a website sample

---
+++ samples/sample_website_service/src/com/tales/samples/websiteservice/WebsiteService.java
@@ -0,0 +1,66 @@
+// ***************************************************************************
+// *  Copyright 2014 Joseph Molnar
+// *
+// *  Licensed under the Apache License, Version 2.0 (the "License");
+// *  you may not use this file except in compliance with the License.
+// *  You may obtain a copy of the License at
+// *
+// *      http://www.apache.org/licenses/LICENSE-2.0
+// *
+// *  Unless required by applicable law or agreed to in writing, software
+// *  distributed under the License is distributed on an "AS IS" BASIS,
+// *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// *  See the License for the specific language governing permissions and
+// *  limitations under the License.
+// ***************************************************************************
+package com.tales.samples.websiteservice;
+
+import java.util.HashMap;
+
+import com.google.common.base.Strings;
+import com.tales.services.http.HttpService;
+import com.tales.services.http.ServiceConstants;
+import com.tales.services.http.WebsiteInterface;
+import com.tales.system.configuration.PropertySource;
+
+/**
+ * A simple website running as a service.
+ * @author Joseph Molnar
+ *
+ */
+public class WebsiteService extends HttpService {
+
+	protected WebsiteService( ) {
+		super( "website_service", "Website Service", "A public tales service show a very simple website." );
+	}
+	
+	@Override
+	protected void onInitializeConfiguration() {
+		String filename = this.getConfigurationManager( ).getStringValue( "settings.file", null ); // get a config filename	 from command-line, if available
+		
+		if( !Strings.isNullOrEmpty( filename ) ) {
+			this.getConfigurationManager( ).addSource( new PropertySource( filename) );
+		}
+	};
+	
+	@Override
+	protected void onStart() {
+		super.onStart();
+		
+		HashMap<String,String> jspInitParameters = new HashMap<String, String>( );
+		jspInitParameters.put( "keepgenerated", "TRUE" );
+		//jspInitParameters.put( "scratchdir", "generated_servlets" );
+
+		WebsiteInterface httpInterface = new WebsiteInterface( ServiceConstants.WEBSITE_INTERFACE_NAME, "website", jspInitParameters, this );
+		
+		this.interfaceManager.register( httpInterface );
+	}
+	
+    public static void main( String[ ] args ) throws Exception {
+    	WebsiteService service = new WebsiteService( );
+    	
+    	service.start( args );
+    	service.run( );
+    	service.stop( );
+	}
+}
